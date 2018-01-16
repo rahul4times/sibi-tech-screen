9f837931598c962fd642b1f15908952446f6c8eb
James Ross <jross@sibipro.com>
2018-01-16 09:02:38 -0700

    init commit

---
+++ src/underbar.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  window._ = {};
+
+  // Call iterator(value, key, collection) for each element of collection.
+  // Accepts both arrays and objects.
+  //
+  // Note: _.each does not have a return value, but rather simply runs the
+  // iterator function over each item in the input collection.
+  _.each = function(collection, iterator) {};
+
+  // Return all elements of an array that pass a truth test.
+  _.filter = function(collection, test) {};
+
+  // Return the results of applying an iterator to each element.
+  _.map = function(collection, iterator) {
+    // map() is a useful primitive iteration function that works a lot
+    // like each(), but in addition to running the operation on all
+    // the members, it also maintains an array of results.
+  };
+})();
