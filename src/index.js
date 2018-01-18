22ae73e807d6709211c2f3d529051ba8599c58e0
rahul4times <tkdmaster.sharma@gmail.com>
2018-01-17 19:00:40 -0700

    part 1 compeleted

--- src/index.js
+++ src/index.js
@@ -8,15 +8,44 @@
   //
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
-  _.each = function(collection, iterator) {};
+
+  _.each = function(collection, iterator) {
+    if(Array.isArray(collection)){
+      for(let i=0; i<collection.length; i++){
+        iterator(collection[i], i, collection)
+      }
+    } else {
+      for(let x in collection){
+        iterator(collection[x], x, collection);
+      }
+    }
+  };
 
   // Return all elements of an array that pass a truth test.
-  _.filter = function(collection, test) {};
+  _.filter = function(collection, test) {
+
+    var myNewArray = [];
+    for(let i=0; i<collection.length; i++){
+      if(test(collection[i])){
+        myNewArray.push(collection[i]);
+      }
+    }
+
+    return myNewArray;
+  };
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+
+    var myNewArray = [];
+    for(let i=0; i<collection.length; i++){
+      myNewArray.push(iterator(collection[i]))
+    }
+
+    return myNewArray;
+  };
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
-  };
+
 })();
